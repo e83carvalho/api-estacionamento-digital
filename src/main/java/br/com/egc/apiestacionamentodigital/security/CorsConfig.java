390492eaa40bb6056f116f7d662cedfc5e7fafe7
ergcarvalho <ergcarvalho@rd.com.br>
2023-02-14 22:10:08 -0300

    reativando cors com host especifico

--- src/main/java/br/com/egc/apiestacionamentodigital/security/CorsConfig.java
+++ src/main/java/br/com/egc/apiestacionamentodigital/security/CorsConfig.java
@@ -1,35 +1,35 @@
-//package br.com.egc.apiestacionamentodigital.security;
-//
-//import java.util.Collections;
-//
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.Ordered;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//	@Bean
-//	public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
-//
-//		CorsConfiguration config = new CorsConfiguration();
-//
-//		config.setAllowedOrigins(Collections.singletonList("*"));
-//		config.setAllowedMethods(Collections.singletonList("*"));
-//		config.setAllowedHeaders(Collections.singletonList("*"));
-//
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", config);
-//
-//		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
-//		bean.setFilter(new CorsFilter(source));
-//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//
-//		return bean;
-//	}
-//
-//}
+package br.com.egc.apiestacionamentodigital.security;
+
+import java.util.Collections;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowedOrigins(Collections.singletonList("https://estacione-digital-fiap.netlify.app"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
+		bean.setFilter(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+		return bean;
+	}
+
+}
